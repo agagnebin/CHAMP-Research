0fc5552cde4660e5f9b0e36be69079dd4ef0f2e5
agagnebin <66806108+agagnebin@users.noreply.github.com>
2021-05-26 15:13:28 -0700

    Delete PHYS_175-T/Homework3 directory

--- PHYS_175-T/Homework3/src/y1.cxx
+++
@@ -1,6 +0,0 @@
-#include "../homework3/y1.h"
-
-int Y11::getY1(int b)
-{
-        return pow(getX(b), 2);
-}
--- PHYS_175-T/Homework3/src/y2.cxx
+++
@@ -1,6 +0,0 @@
-#include "../homework3/y2.h"
-
-int Y21::getY2(int c)
-{
-        return pow(getX(c), 3);
-}
